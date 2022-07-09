608677634398e75989dc113c073720b01159c08b
TylerCoplan <tyler.coplan@snhu.edu>
2022-07-09 10:28:52 -0400

    updated file structure for MVC routing, created header/footer partials

--- app.js
+++ app.js
@@ -3,20 +3,20 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const hbs = require('hbs');
 
 const indexRouter = require('./app_server/routes/index');
 const usersRouter = require('./app_server/routes/users');
 const travelRouter = require('./app_server/routes/travel');
-const { hasSubscribers } = require('diagnostics_channel');
 
 const app = express();
-
 // view engine setup
 app.set('views', path.join(__dirname, 'app_server', 'views'));
-app.set('view engine', 'hbs');
 
 // register handlebars partials (https://www.npmjs.com/package/hbs)
-hbs.registerPatials(path.join(__dirname, 'app_server', 'views/partials'));
+hbs.registerPartials(path.join(__dirname, 'app_server', 'views/partials'));
+
+app.set('view engine', 'hbs');
 
 app.use(logger('dev'));
 app.use(express.json());
