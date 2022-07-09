7e92a8dc3aa0a64f1213f0b28cc2140379de93da
TylerCoplan <tyler.coplan@snhu.edu>
2022-07-09 09:43:30 -0400

    updated folder structure for hbs engine, created header/footer partials

--- app.js
+++ app.js
@@ -1,17 +1,22 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const indexRouter = require('./app_server/routes/index');
+const usersRouter = require('./app_server/routes/users');
+const travelRouter = require('./app_server/routes/travel');
+const { hasSubscribers } = require('diagnostics_channel');
 
-var app = express();
+const app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, 'app_server', 'views'));
 app.set('view engine', 'hbs');
+
+// register handlebars partials (https://www.npmjs.com/package/hbs)
+hbs.registerPatials(path.join(__dirname, 'app_server', 'views/partials'));
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -21,6 +26,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/travel', travelRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
