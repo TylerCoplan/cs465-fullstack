3a0a3a2ec0b8e0fdc7b1ca2038fd6622402b6711
TylerCoplan <tyler.coplan@snhu.edu>
2022-07-12 20:11:52 -0400

    fixed footer paths, added data json objects, created hbs html templates

--- app_server/controllers/travel.js
+++ app_server/controllers/travel.js
@@ -1,6 +1,9 @@
+const fs = require('fs');
+const trips = JSON.parse(fs.readFileSync('./data/trips.json', 'utf8'));
+
 /* GET travel view */
 const travel = (req, res) => {
-    res.render('travel', { title: 'Travlr Getaways' });
+    res.render('travel', { title: 'Travlr Getaways', trips });
 };
 
 module.exports = {
